6eff86e314d7903b69fc2b0eaed99046f2c80359
João Victor <97251437+joaofvianna@users.noreply.github.com>
2022-07-11 17:39:39 -0300

    Exercícios JavaScript (Parte 3)

---
+++ aula12ex/ex015/sript.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano= document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <10) {
+                // criança
+                img.setAttribute('src', 'baby-m.png')
+            } else if (idade >= 10 && idade < 21) {
+                // jovem
+                img.setAttribute('src', 'homem-jovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'homem-idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <10) {
+                // criança
+                img.setAttribute('src', 'baby-f.png')
+            } else if (idade >= 10 && idade < 21) {
+                // jovem
+                img.setAttribute('src', 'mulher-jovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'mulher-idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
